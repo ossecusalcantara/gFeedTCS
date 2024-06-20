c5a787120fe7a14ea2c4ddcf66080238aab1c1a9
ossecusalcantara <adson.s.alcantara@gmail.com>
2024-06-19 23:44:37 -0300

    Corrigindo app URL

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://g-feed-tcs-1fc10ccde268.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL'),
 
