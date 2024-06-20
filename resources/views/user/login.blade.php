dd2c2a323d65cff07406575dcca29bf5dec22e63
ossecusalcantara <adson.s.alcantara@gmail.com>
2024-06-20 07:03:34 -0300

    Correção https

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -69,7 +69,7 @@
                                     @endif
 
                                     {!! Form::open([
-                                        'route' => secure_url('user.login'),
+                                        'route' => 'user.login',
                                         'method' => 'post',
                                         'class' => 'row g-3 needs-validation',
                                         'novalidate',
